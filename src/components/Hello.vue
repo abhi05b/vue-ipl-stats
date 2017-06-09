64c63b343efaea25331c43809bf3296487baf415
Abhishek Singh <abhi05b@gmail.com>
2017-06-09 19:45:47 +0530

    ui changes

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -17,7 +17,14 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <button class="ui primary button">
+      Save
+    </button>
+    <button class="ui button">
+      Discard
+    </button>
   </div>
+
 </template>
 
 <script>
